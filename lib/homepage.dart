f0046afac0f2b3ea577b5b2eeb1fe0ae6a0b11ea
nandini <Jahedalam9275@gmail.com>
2021-07-09 02:05:11 +0600

    day 1 completed

---
+++ lib/homepage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = 'codepur';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MY APP'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                print('search clicked');
+              },
+              icon: Icon(Icons.search)),
+          IconButton(
+              onPressed: () {
+                print('profile clicked');
+              },
+              icon: Icon(Icons.person)),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          child: Text("This is $days of tutorial of $name"),
+        ),
+      ),
+      drawer: Drawer(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text('button 1'),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red)),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,14 @@
 import 'package:flutter/material.dart';
+import 'homepage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-          centerTitle: true,
-          actions: [
-            IconButton(
-                onPressed: () {
-                  print('search clicked');
-                },
-                icon: Icon(Icons.search)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.person))
-          ],
-        ),
-        
-      ),
+      home: HomePage(),
     );
   }
 }
